a8b6834e146f0fdb2bdfd927d97e8ef8b1f423ed
Taylor Sieling <taylorasieling@gmail.com>
2020-08-29 17:52:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,16 +68,9 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   
   holiday_hash.each do |season, holiday|
-    puts #{season}
-    holiday.each do |holiday, supply|
-      
-      
-      
-      holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
-    holiday.each do |holiday, supplies|
-    puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
-      
+    holiday.each do |holiday, supply|
+      puts " #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
     end
   end
 end
