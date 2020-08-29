4bd0f86a96734d27222900577b326465c1f18bb9
Taylor Sieling <taylorasieling@gmail.com>
2020-08-29 17:44:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,6 +83,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, supply|
      holiday if supply.include?("BBQ")
+      holiday.flatten
       end
     end
   end
